dd54089cd7c770f10f1d3b0260656d1f93dcc7f1
KD1712 <17khanjan@gmail.com>
2023-06-28 11:25:57 +0530

    Car Showcase

---
+++ components/index.ts
@@ -0,0 +1,20 @@
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import Searchbar from "./Searchbar";
+import Customfilter from "./Customfilter";
+import SearchManufacturer from "./SearchManufacturer";
+import CarDetails from "./CarDetails";
+import CarCard from "./CarCard";
+export {
+  Hero,
+  CustomButton,
+  CarCard,
+  Navbar,
+  Footer,
+  Searchbar,
+  Customfilter,
+  SearchManufacturer,
+  CarDetails,
+};
